8cabae062b545c8817f22c103dfcf21e4e75b72b
SERGIO ACOSTA <42353861+sergio2000@users.noreply.github.com>
2022-06-02 19:20:22 -0700

    Updates

---
+++ precache-manifest.4c040394947e15a64b119181f63afd5b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd97f040f909da82b27d",
+    "url": "/static/css/main.43bd75d5.chunk.css"
+  },
+  {
+    "revision": "fd97f040f909da82b27d",
+    "url": "/static/js/main.fd97f040.chunk.js"
+  },
+  {
+    "revision": "c24d8e46d90600f6074c",
+    "url": "/static/js/1.c24d8e46.chunk.js"
+  },
+  {
+    "revision": "2d146a908548c0816272",
+    "url": "/static/css/2.1e2dfc6e.chunk.css"
+  },
+  {
+    "revision": "2d146a908548c0816272",
+    "url": "/static/js/2.2d146a90.chunk.js"
+  },
+  {
+    "revision": "2196b30ba0e585426b96",
+    "url": "/static/js/runtime~main.2196b30b.js"
+  },
+  {
+    "revision": "f6a4fee9dc37e09d3bc464781ee72cf6",
+    "url": "/static/media/planorama_logo.f6a4fee9.png"
+  },
+  {
+    "revision": "ce3cb833711a9ea98e56a4d365e97b75",
+    "url": "/static/media/requirements-tracing.ce3cb833.png"
+  },
+  {
+    "revision": "5af6ac2efe360023d743fc6d5b12687e",
+    "url": "/static/media/library.5af6ac2e.png"
+  },
+  {
+    "revision": "68a3bc1c2feb39c9e8ae07d1d7f01b89",
+    "url": "/static/media/profile.68a3bc1c.jpeg"
+  },
+  {
+    "revision": "3bf4d9a211d6130a69911adc70830034",
+    "url": "/static/media/clima.3bf4d9a2.png"
+  },
+  {
+    "revision": "7bb72c2290e24b7015a51ecc6c15af4a",
+    "url": "/static/media/error.7bb72c22.mp4"
+  },
+  {
+    "revision": "291a4708795b2bf8205488ce59859d50",
+    "url": "/index.html"
+  }
+];
